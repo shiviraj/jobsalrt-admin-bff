b2426184ac0d430bd3c2d6e89b2ea27f73d390d8
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-05-21 07:46:55 +0530

    handled unauthorized error while login.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -18,7 +18,10 @@ const UserController = () => {
 
   router.post('/sign-in', (req, res) => {
     UserService.signIn(req.body)
-      .then(response => res.send(response.data))
+      .then(response => {
+        if (!response.data.token) return handleError({response: {status: 401}}, res, USER_CUSTOM_ERRORS.UNAUTHORIZED)
+        return res.send(response.data);
+      })
       .catch(error => {
         logger.logAPIError(req, error, USER_CUSTOM_ERRORS.UNAUTHORIZED)
         handleError(error, res, USER_CUSTOM_ERRORS.UNAUTHORIZED)
