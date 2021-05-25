b2a890dac8051a52f1c9f662547c216eb736c742
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-05-25 20:22:34 +0530

    Added mechanism to awake all ther servers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from './app'
 import logger from './logging/logger'
+import axios from "axios";
 
 const BFF_PORT = process.env.PORT || 3001
 const BFF_HOST = "localhost"
@@ -8,3 +9,11 @@ app.listen(BFF_PORT, () => {
   const url = `http://${BFF_HOST}:${BFF_PORT}`
   logger.info(`BFF server is ready at: ${url} 🔥🔥🔥`)
 })
+
+setInterval(() => {
+  axios.post("https://jobsalrt-admin.herokuapp.com/api/user/sign-in", {
+    email: "example@email.com",
+    password: "password"
+  })
+    .catch(() => ({}))
+}, 600000)
