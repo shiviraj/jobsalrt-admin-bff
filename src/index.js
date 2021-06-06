55824cfb9ceedd861a8f69436f0089d2d2da17c6
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-06-06 08:29:49 +0530

    Added mechanism to restart the jobsalrt post updater server.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import {exec} from "child_process";
+import axios from "axios";
 import app from './app'
 import logger from './logging/logger'
-import axios from "axios";
 
 const BFF_PORT = process.env.PORT || 3001
-const BFF_HOST = "localhost"
+const BFF_HOST = process.env.HOST || "localhost"
 
 app.listen(BFF_PORT, () => {
   const url = `http://${BFF_HOST}:${BFF_PORT}`
   logger.info(`BFF server is ready at: ${url} 🔥🔥🔥`)
 })
+
+const command = `export HEROKU_API_KEY=${process.env.HEROKU_API_KEY}; heroku restart -a ${process.env.HEROKU_APP_NAME}`
 
 setInterval(() => {
   axios.post("https://jobsalrt-admin.herokuapp.com/api/user/sign-in", {
@@ -21,7 +24,6 @@ setInterval(() => {
   if (![0, 6].includes(date.getUTCDay()) && [10, 11].includes(date.getUTCHours()))
     axios.get("https://stock-market-alert.herokuapp.com/").catch(() => ({}))
 
-  if (date.getUTCHours() % 2) {
-    axios.get("https://jobsalrt-post-updater.herokuapp.com/").catch(() => ({}))
-  }
+  exec(command);
+
 }, 600000)
